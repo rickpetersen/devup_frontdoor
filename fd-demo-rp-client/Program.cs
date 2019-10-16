515d37bbd9c7f08515d1cc640a405e54af441d31
Rick Petersen <dape@microsoft.com>
2019-10-16 08:41:24 -0500

    Initial commit

---
+++ fd-demo-rp-client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace fd_demo_rp_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var iterations = 49;
+            string url = string.Empty;
+            long totalMS = 0;
+
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+            
+            if (url == string.Empty)
+            {
+                throw new ArgumentException("Please provide an endpoint");
+            }
+            var timer = new Stopwatch();
+            HttpResponseMessage file;
+            using (var client = new HttpClient())
+            {
+                //loop through n times and grab file
+                for (var i = 0; i <= iterations; i++)
+                {
+
+                    timer.Start();
+                    using (file = await client.GetAsync(url))
+                    {
+                        timer.Stop();
+                        totalMS += timer.ElapsedMilliseconds;
+                        Console.WriteLine($"Getting file {i + 1} : Elapsed - {timer.ElapsedMilliseconds}ms");
+                        timer.Reset();
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Average transfer time: {(totalMS / iterations)}ms");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fd-demo-rp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fd_demo_rp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
